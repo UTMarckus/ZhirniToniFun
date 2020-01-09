dea3bdb9e5378138a5e9b77aa3b0e1f6621ffde4
'UTMarckus' <'utmarckus@gmail.com'>
2020-01-09 12:36:19 +0200

    Formatter

--- src/dick.java
+++ src/dick.java
@@ -1,50 +1,29 @@
-import java.io.File;
+import java.util.Formatter;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 public class dick {
 	
-	static String[][] m = new String[5][3];
+	static Formatter x;
 	static Scanner scn;
 
 	public static void main(String[] args) {
 		
-		openFile();
-		readFile();
-		out();
-		
-	}
-
-	private static void out() {
-		
-		for(int row=0; row<m.length; row++) {
-			for(int col=0; col<m[row].length; col++) {
-				System.out.print(m[row][col] + "  ");
-			}
-			System.out.println();
-		}
-		
-	}
-
-	private static void readFile() {
-		
-		while(scn.hasNext()) {
-			for(int row=0; row<m.length; row++) {
-				for(int col=0; col<m[row].length; col++) {
-					m[row][col] = scn.next();
-				}
-			}
-		}
-		
-	}
-
-	private static void openFile() {
-		
 		try {
-			scn = new Scanner(new File("res//1.txt"));
-		}catch(Exception exc) {
-			JOptionPane.showMessageDialog(null, "Файл не найден");
-		}
+			
+			x = new Formatter("res//1.txt");
+			scn = new Scanner(System.in);
+			
+			System.out.println("Сколько вам лет?");
+			int a = (int)Double.parseDouble(scn.next());
+			System.out.println("Как вас зовут?");
+			String b = scn.next();
+			System.out.println("Где вы живете?");
+			String c = scn.next();
+			
+			x.format("Меня зовут %s, мне %d лет, я живу в городе %s", b, a, c);
+			x.close();
+			
+		}catch(Exception exc) {}
 		
 	}
 }
