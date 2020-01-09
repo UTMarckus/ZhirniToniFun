259d43c2515ac3ded5982e65639bdbf2dcc3dfe6
'UTMarckus' <'utmarckus@gmail.com'>
2020-01-09 11:36:46 +0200

    New Dick) Read File

--- src/dick.java
+++ src/dick.java
@@ -1,14 +1,50 @@
-import javax.swing.JFrame;
+import java.io.File;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-class dick {
+public class dick {
+	
+	static String[][] m = new String[5][3];
+	static Scanner scn;
 
 	public static void main(String[] args) {
 		
-		Reader r = new Reader("G");
-		r.setVisible(true);
-		r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		r.setSize(300, 200);
-		r.setResizable(false);
-		r.setLocationRelativeTo(null);
+		openFile();
+		readFile();
+		out();
+		
+	}
+
+	private static void out() {
+		
+		for(int row=0; row<m.length; row++) {
+			for(int col=0; col<m[row].length; col++) {
+				System.out.print(m[row][col] + "  ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+	private static void readFile() {
+		
+		while(scn.hasNext()) {
+			for(int row=0; row<m.length; row++) {
+				for(int col=0; col<m[row].length; col++) {
+					m[row][col] = scn.next();
+				}
+			}
+		}
+		
+	}
+
+	private static void openFile() {
+		
+		try {
+			scn = new Scanner(new File("res//1.txt"));
+		}catch(Exception exc) {
+			JOptionPane.showMessageDialog(null, "Файл не найден");
+		}
+		
 	}
 }
